a42f10ccbf4dc4e3a2d2ffc32ec96d805fba961b
Jessica Mallian <jlmallian@gmail.com>
2018-07-18 19:07:24 -0500

    Initial Commit

---
+++ ChooseYourOwnAdventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChooseYourOwnAdventure
+//
+//  Created by Jessica Mallian on 7/18/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

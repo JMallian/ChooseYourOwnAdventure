4fe94f0ede3d1804ee220b08ed30d3fb39fd0046
Jessica Mallian <jlmallian@gmail.com>
2018-07-19 19:16:15 -0500

    created MYOAViewController and added a navigationItem to it

---
+++ ChooseYourOwnAdventure/MYOAViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MYOAViewController.swift
+//  ChooseYourOwnAdventure
+//
+//  Created by Jessica Mallian on 7/19/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import UIKit
+class MYOAViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+    }
+    
+    @objc func startOver() {
+        
+    }
+}
